7663670597426020cc1dd2932c79d4522c99ae3b
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-09 23:18:56 +0530

    Modified : Variable name that stores randomly generated attendance status.

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,7 +1,7 @@
 const IS_PRESENT = 1;
-var attendance = Math.floor(Math.random() * 10) % 2;
+var attendanceStatus = Math.floor(Math.random() * 10) % 2;
 
-if (attendance == IS_PRESENT)
+if (attendanceStatus == IS_PRESENT)
     console.log("Employee is PRESENT!");
 else
     console.log("Employee is ABSENT!");
