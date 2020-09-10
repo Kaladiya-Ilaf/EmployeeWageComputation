355d29aacd20ea0799021edb8ccd673f51f56011
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-10 07:21:07 +0530

    Added : Code to calculte employee daily wages based on working hours.

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,9 +1,9 @@
 const IS_PRESENT = 1;
-
-var employeeWorkingHours;
+const WAGE_PER_HOUR = 20;
 
 function checkEmployeeAttendance() {
     var attendanceStatus = Math.floor(Math.random() * 10) % 2;
+    var employeeWorkingHours;
 
     if (attendanceStatus == IS_PRESENT) {
         console.log("Employee is PRESENT!");
@@ -15,4 +15,10 @@ function checkEmployeeAttendance() {
     return employeeWorkingHours;
 }
 
-var workingHours = checkEmployeeAttendance();
+function calculateEmployeeWage(workHours) {
+    return WAGE_PER_HOUR * workHours;
+}
+
+workingHours = checkEmployeeAttendance();
+dailyWage = calculateEmployeeWage(workingHours)
+console.log("Wage : " + dailyWage);
