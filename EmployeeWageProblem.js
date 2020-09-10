b5fe7d4680fbe8bfd0f1001dc1b97ab9e7780704
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-10 07:02:37 +0530

    Added : Code to compute working hours of employee based on its attendance

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,7 +1,18 @@
 const IS_PRESENT = 1;
-var attendanceStatus = Math.floor(Math.random() * 10) % 2;
 
-if (attendanceStatus == IS_PRESENT)
-    console.log("Employee is PRESENT!");
-else
-    console.log("Employee is ABSENT!");
+var employeeWorkingHours;
+
+function checkEmployeeAttendance() {
+    var attendanceStatus = Math.floor(Math.random() * 10) % 2;
+
+    if (attendanceStatus == IS_PRESENT) {
+        console.log("Employee is PRESENT!");
+        employeeWorkingHours = 8;
+    } else {
+        console.log("Employee is ABSENT!");
+        employeeWorkingHours = 0;
+    }
+    return employeeWorkingHours;
+}
+
+var workingHours = checkEmployeeAttendance();
