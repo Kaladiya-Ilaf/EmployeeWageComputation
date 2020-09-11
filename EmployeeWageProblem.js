8184b7b6a38c4efbbb0569a95ef39588fecac3b7
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-11 11:22:54 +0530

    Modified : Code to split attendance and get hours functions

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -4,26 +4,25 @@ const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAXIMUM_WORKING_HOURS = 100;
 
+var days = 0, totalWage = 0, totalWorkingHours = 0;
 var employeeDailyWage = new Array();
 
-function checkEmployeeAttendance() {
-    var attendanceStatus = Math.floor(Math.random() * 10) % 3;
-    var employeeWorkingHours;
+function checkEmployeeAttendance(){
+    return attendanceStatus = Math.floor(Math.random() * 10) % 3;
+}
 
-    switch (attendanceStatus) {
+function getEmployeeWorkingHours(employeeAttendance) {
+    switch (employeeAttendance()) {
         case IS_PART_TIME:
             employeeWorkingHours = 4;
             break;
-
         case IS_FULL_TIME:
             employeeWorkingHours = 8;
             break;
-
         default:
             employeeWorkingHours = 0;
             break;
     }
-
     return employeeWorkingHours;
 }
 
@@ -31,18 +30,15 @@ function calculateEmployeeWage(workHours) {
     return WAGE_PER_HOUR * workHours;
 }
 
-days = 0;
-totalWage = 0;
-totalWorkingHours = 0;
-
 while (days < NUM_OF_WORKING_DAYS && totalWorkingHours <= MAXIMUM_WORKING_HOURS) {
-    workingHours = checkEmployeeAttendance();
+    workingHours = getEmployeeWorkingHours(checkEmployeeAttendance);
     totalWorkingHours += workingHours;
     dailyWage = calculateEmployeeWage(workingHours)
     employeeDailyWage.push(dailyWage);
     totalWage += dailyWage;
     days += 1;
 }
+
 employeeDailyWage.forEach(element => console.log(element));
 console.log(`Total working hours : ${totalWorkingHours}`);
 console.log(`Wage : ${totalWage}`);
